6adeb989c838ad3a8dee4424a84140867331a0ea
georgikbarsemian <georgik.barsemian@mail.concordia.ca>
2019-02-28 17:42:19 -0500

    Creation of a serivice layer to figure out the userType we have at login

--- src/main/java/application/controller/LoginController.java
+++ src/main/java/application/controller/LoginController.java
@@ -1,13 +1,22 @@
 package application.controller;
 
+import application.Service.LoginAuthentication;
+import application.model.Patient;
+import application.repository.PatientRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
+import java.util.Collection;
+
 @Controller
 public class LoginController {
+    @Autowired
+    private PatientRepository patientRepository;
+
     @RequestMapping(value = "/login", method = RequestMethod.GET)
     public String loginPage(){
         return "login";
@@ -15,7 +24,10 @@ public class LoginController {
 
     @PostMapping(value = "/validate")
     public String loginPagelog(@ModelAttribute LoginForm loginForm){
-        System.out.println("********************************"+loginForm.getIdentification()+"   "+loginForm.getPassword());
+        LoginAuthentication loginAuthentication = new LoginAuthentication(loginForm.getIdentification(), loginForm.getPassword());
+
+        System.out.println("********************************"+loginForm.getIdentification()+"   "+loginForm.getPassword()+"\n"+
+                loginAuthentication.getUserType());
         return "home";
     }
 
