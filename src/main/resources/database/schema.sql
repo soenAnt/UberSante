cb08238772c8dba6026e12a81d2584f3c2aeacf5
Keeran10 <keeran.r10@gmail.com>
2019-03-01 18:45:26 -0500

    #3 added back description field

--- src/main/resources/database/schema.sql
+++ src/main/resources/database/schema.sql
@@ -48,6 +48,7 @@ CREATE TABLE IF NOT EXISTS appointments (
   `start_time` TIME NOT NULL,
   `end_time` TIME NOT NULL,
   `appointment_type` VARCHAR(10) NOT NULL,
+  `description` VARCHAR(255) NULL,
   FOREIGN KEY (patient_id) REFERENCES patients(user_id)
 ) ENGINE=InnoDB;
 
