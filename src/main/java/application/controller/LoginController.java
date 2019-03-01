43064852c2aa7aacda986859af2fe496429bac94
georgikbarsemian <georgik.barsemian@mail.concordia.ca>
2019-02-28 19:37:21 -0500

    Validating the user based on the password and the identication process

--- src/main/java/application/controller/LoginController.java
+++ src/main/java/application/controller/LoginController.java
@@ -4,9 +4,11 @@ import application.Service.LoginAuthentication;
 import application.model.Doctor;
 import application.model.Nurse;
 import application.model.Patient;
+import application.model.User;
 import application.repository.DoctorRepository;
 import application.repository.NurseRepository;
 import application.repository.PatientRepository;
+import application.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.ModelAttribute;
@@ -24,6 +26,10 @@ public class LoginController {
 
     @Autowired
     private NurseRepository nurseRepository;
+    @Autowired
+    private UserRepository userRepository;
+    @Autowired
+    private PatientRepository patientRepository;
 
     @RequestMapping(value = "/login", method = RequestMethod.GET)
 
@@ -39,12 +45,40 @@ public class LoginController {
     @PostMapping(value = "/validate")
     public String loginPagelog(@ModelAttribute LoginForm loginForm){
         LoginAuthentication loginAuthentication = new LoginAuthentication(loginForm.getIdentification(), loginForm.getPassword());
+
         System.out.println("********************************"+loginForm.getIdentification()+"   "+loginForm.getPassword()+"\n"+
                 loginAuthentication.getUserType()+" REAL:"+loginAuthentication.getRealAuthenticaton());
-        String real = loginAuthentication.getRealAuthenticaton();
-        Nurse nurse = nurseRepository.findByAccessId(real);
-        System.out.println("***********++++++++++++++++++++****"+nurse);
+
+       boolean validate = validateUser(loginAuthentication.getRealAuthenticaton(), loginAuthentication.getPassword(),loginAuthentication.getUserType());
+
         return "home";
+    }
+
+    private boolean validateUser(String identification, String password, int userType){
+        Doctor doctor = null;
+        Nurse nurse = null;
+        Collection<Patient> patient  = null;
+
+        // user type 1 is a doctor, type 2 a nurse and type 3 a patient
+        switch(userType){
+            case 1: doctor = doctorRepository.findByPhysicianPermitNumber(Integer.parseInt(identification));
+                break;
+            case 2: nurse = nurseRepository.findByAccessId(identification);
+                break;
+            case 3: patient = patientRepository.findByEmail(identification);
+                break;
+        }
+
+        User user = userRepository.findByPassword(password);
+        System.out.println("**************************--------------------------------------------");
+        System.out.println("D:"+doctor+ " N:"+nurse+" P:" + patient+ " U:"+ user);
+        if(doctor != null || nurse != null | patient != null) {
+            System.out.println("************************** MADE ITTTT");
+            if (user != null) {
+                return true;
+            }
+        }
+        return false;
     }
 
 
