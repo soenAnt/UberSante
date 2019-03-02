776e6aa5c218fe8b637c93ccd00471abf4d35b3f
georgikbarsemian <georgik.barsemian@mail.concordia.ca>
2019-03-01 21:17:52 -0500

    the Doctor can be redirected to his page of creating a schedule after being directed to the home page

--- src/main/java/application/controller/DoctorScheduleController.java
+++ src/main/java/application/controller/DoctorScheduleController.java
@@ -2,11 +2,13 @@ package application.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 
 @Controller
 public class DoctorScheduleController {
-    @GetMapping("/schedule")
-    public String schedulePage() {
+    @GetMapping(path="/schedule/{someID}")
+    public String schedulePage(@PathVariable String someID) {
+        System.out.println("*******************************"+someID);
         return "errorLogin";
     }
 }
