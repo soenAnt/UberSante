495afa59fa8973e00819c8d1096a8bd687451351
Chen <c_xi20@live.concordia.ca>
2019-03-11 00:31:26 -0400

    feature 4 #8
    Finding available doctor and room for checkoutAppointment method

--- src/main/java/application/repository/BookingRepository.java
+++ src/main/java/application/repository/BookingRepository.java
@@ -6,9 +6,12 @@ import application.model.Booking;
 import application.model.Doctor;
 import application.model.Patient;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
+import java.sql.Time;
 import java.util.Collection;
+import java.util.Date;
 
 @Repository
 public interface BookingRepository extends JpaRepository<Booking, Integer>{
@@ -16,4 +19,11 @@ public interface BookingRepository extends JpaRepository<Booking, Integer>{
     Collection<Booking> findByPatient(Patient patient);
 
     Collection<Booking> findByDoctor(Doctor doctor);
+    
+    @Query("SELECT room FROM bookings INNER JOIN appointments "
+    		+ "WHERE bookings.appointment_id = appointments.appointment_id "
+    		+ "AND date=?1 "
+    		+ "AND start_time <= ?2 "
+    		+ "AND end_time >= ?3")
+    Collection<Integer> findTakenRooms(Date date, Time start_time, Time end_time);
 }
